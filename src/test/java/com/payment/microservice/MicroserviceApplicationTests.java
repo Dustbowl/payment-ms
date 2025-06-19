38732ad2485fe3fb1b0ca5639299c2a4a886f4a8
Dustbowl <vuonganthony@hotmail.com>
2025-06-19 03:12:23 -0500

    added jenkins and docker

--- src/test/java/com/payment/microservice/MicroserviceApplicationTests.java
+++ src/test/java/com/payment/microservice/MicroserviceApplicationTests.java
@@ -3,7 +3,9 @@ package com.payment.microservice;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import com.payment.ms.PaymentMsApplication;
+
+@SpringBootTest(classes = PaymentMsApplication.class)
 class MicroserviceApplicationTests {
 
 	@Test
